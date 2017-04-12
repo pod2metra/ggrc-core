6bff2cc0ec0ee4aa4dd6110d1f254d3e9c6d9e78
pod2metra <pod2metra@gmail.com>
2017-04-12 17:42:17 +0300

    Add reindex query option for cycle task comments

--- src/ggrc_workflows/models/cycle_task_group_object_task.py
+++ src/ggrc_workflows/models/cycle_task_group_object_task.py
@@ -273,7 +273,11 @@ class CycleTaskGroupObjectTask(
             "email",
             "name",
             "id"
-        )
+        ),
+        orm.Load(cls).subqueryload("cycle_task_entries").load_only(
+            "description",
+            "id"
+        ),
     )
 
 
