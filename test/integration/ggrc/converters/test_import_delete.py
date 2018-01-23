4fbdb9866122cd8bba3c78f792d2a422874be850
pod2metra <pod2metra@gmail.com>
2018-01-23 04:03:52 +0300

    Add assert revision shouldn't be deleted cascadely

--- test/integration/ggrc/converters/test_import_delete.py
+++ test/integration/ggrc/converters/test_import_delete.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2018 Google Inc.
 # Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
 
+from ggrc.models import all_models
+
 from integration.ggrc import TestCase
 
 
@@ -12,10 +14,13 @@ class TestBasicCsvImport(TestCase):
 
   def test_policy_basic_import(self):
     filename = "ca_setup_for_deletion.csv"
+    revision_ids = [i.id for i in all_models.Revision.query]
     self.import_file(filename)
-
     filename = "ca_deletion.csv"
     response_data = self.import_file(filename)
-
+    query = all_models.Revision.query.filter(
+        all_models.Revision.id.in_(revision_ids),
+    )
+    self.assertEqual(len(revision_ids), len([i.id for i in query]))
     self.assertEqual(response_data[0]["deleted"], 2)
     self.assertEqual(response_data[0]["ignored"], 0)
