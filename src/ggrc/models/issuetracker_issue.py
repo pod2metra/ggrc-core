430b309a0e7e7aca448ce5a8609d39dc5c2d2e68
pod2metra <pod2metra@gmail.com>
2018-04-12 15:56:36 +0300

    Make issuetracker_issue single item only

--- src/ggrc/models/issuetracker_issue.py
+++ src/ggrc/models/issuetracker_issue.py
@@ -201,4 +201,5 @@ class IssueTracked(object):
         IssuetrackerIssue,
         primaryjoin=join_function,
         backref="{}_issue_tracked".format(cls.__name__),
+        uselist=False,
     )
